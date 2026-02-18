eca8d1abdc5f0f4d8b165538bc7cb2aa79bd4522
Ben <benharris@manx.net>
2026-02-18 19:13:40 +0000

    initial scaffolding

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>Crossword Game Under Construction</h1>
+
+      <p className="footer">
+        Made by <a href="https://docs.nouse.co.uk/pages/15%20tech-team.html">Nouse Tech</a>
+      </p>
+    </>
+  )
+}
+
+export default App
