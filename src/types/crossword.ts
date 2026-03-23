c4c36e9105a831dabf4b798231a4f0489e16098c
Yakup Ozturk <yakup.ozturk0905@gmail.com>
2026-03-23 17:14:37 +0000

    Nouse Crossword Game version 1.0.0 - Built the first version of the game

--- src/types/crossword.ts
+++ src/types/crossword.ts
@@ -1,8 +1,8 @@
 export interface CrosswordPuzzle {
     title: string;
-    size: [number, number];
-    workingGrid: (number | string)[];
-    answerGrid: (number | string)[];
+    size: number;
+    workingGrid: string[];
+    answerGrid: string[];
     hints: Hints;
 }
 
@@ -15,21 +15,4 @@ export interface ClueEntry {
 export interface Hints {
     across: ClueEntry[];
     down: ClueEntry[];
-}
-
-export interface Guesses {
-    [cellNum: number]: string;
-}
-
-export interface ValidationResult {
-    isCorrect: boolean;
-    errors: string[];
-}
-
-export interface Cell {
-    row: number;
-    col: number;
-    value: string;
-    isBlocked: boolean;
-    clueNumber?: number;
 }
